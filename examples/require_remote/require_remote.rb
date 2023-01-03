0809f84d267d3f65a02b108a5dd4da41b97b6e0a
adam429 <adam429.lee@gmail.com>
2023-01-03 16:53:17 +0800

    require remote refactor

--- examples/require_remote/require_remote.rb
+++ examples/require_remote/require_remote.rb
@@ -1,5 +1,5 @@
 # load library remotely by require_remote
-require 'require_remote'
+require 'smartbook/require_remote'
 
 # require file from https://raw.githubusercontent.com/adam429/smart-book/main/examples/require_remote/lib/math_lib.rb
 require_remote '@adam429/smart-book/examples/require_remote/lib/math_lib'
